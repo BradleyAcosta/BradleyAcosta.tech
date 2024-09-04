754539ac7da23a30443717763e1639842d3e917c
Bradley Acosta Rojas <thescrill12@gmail.com>
2024-09-03 22:43:15 -0400

    Added projects on the file

---
+++ projects.js
@@ -0,0 +1,42 @@
+// projects.js
+const apiUrl = 'https://api.github.com/users/BradleyAcosta/repos';
+const token = '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';
+
+fetch(apiUrl, {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then(response => response.json())
+  .then(data => {
+    const projects = data.map(repo => {
+      return {
+        name: repo.name,
+        description: repo.description,
+        url: repo.html_url,
+        avatar_url: repo.owner.avatar_url,
+      };
+    });
+    displayProjects(projects);
+  })
+  .catch(error => console.error(error));
+
+function displayProjects(projects) {
+  const projectList = document.getElementById('project-list');
+  projects.forEach(project => {
+    const projectItem = document.createElement('li');
+    projectItem.innerHTML = `
+      <div class="project-card" style="background-image: url(${project.avatar_url})">
+        <h2>${project.name}</h2>
+        <p>${project.description}</p>
+      </div>
+    `;
+    projectItem.onclick = () => {
+      window.location.href = project.url;
+    };
+    projectList.appendChild(projectItem);
+  });
+}
+
+
+
